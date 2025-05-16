12c4b665aeca5811ffaa4b1fe26e3b556b560438
hodifa <databaserepos@gmail.com>
2025-05-16 20:27:41 +0200

    fix render in html

--- components/render-html.tsx
+++ components/render-html.tsx
@@ -10,7 +10,8 @@ const RenderHtml = ({ html, className }: Props) => {
     <div
       className={cn("ProseMirror tiptap rounded-lg max-w-full", className)}
       dangerouslySetInnerHTML={{
-        __html: wrapTablesAndImages(html),
+        // __html: wrapTablesAndImages(html),
+        __html: html,
       }}
     />
   );
@@ -18,32 +19,32 @@ const RenderHtml = ({ html, className }: Props) => {
 
 export default RenderHtml;
 
-// Function to wrap tables and group consecutive images
-function wrapTablesAndImages(html: string): string {
-  let updatedHtml = html;
+// // Function to wrap tables and group consecutive images
+// function wrapTablesAndImages(html: string): string {
+//   let updatedHtml = html;
 
-  // Wrap <table> elements
-  updatedHtml = updatedHtml.replace(
-    /<table([\s\S]*?)<\/table>/gi,
-    (match) => `<div class="tableWrapper">${match}</div>`
-  );
+//   // Wrap <table> elements
+//   updatedHtml = updatedHtml.replace(
+//     /<table([\s\S]*?)<\/table>/gi,
+//     (match) => `<div class="tableWrapper">${match}</div>`
+//   );
 
-  // Group only two or more consecutive <img> elements
-  updatedHtml = updatedHtml.replace(/(<img[\s\S]*?>\s*){2,}/gi, (match) => {
-    // Extract all <img> tags and apply custom styling
-    const images =
-      match
-        .match(/<img[\s\S]*?>/g) // Extract individual <img> tags
-        ?.map((img) =>
-          img.replace(
-            /<img([\s\S]*?)>/,
-            `<img $1 class="customImage rounded-lg shadow-md max-w-full w-full h-full" loading="lazy" />`
-          )
-        )
-        .join("") || "";
+//   // Group only two or more consecutive <img> elements
+//   updatedHtml = updatedHtml.replace(/(<img[\s\S]*?>\s*){2,}/gi, (match) => {
+//     // Extract all <img> tags and apply custom styling
+//     const images =
+//       match
+//         .match(/<img[\s\S]*?>/g) // Extract individual <img> tags
+//         ?.map((img) =>
+//           img.replace(
+//             /<img([\s\S]*?)>/,
+//             `<img $1 class="customImage rounded-lg shadow-md max-w-full w-full h-full" loading="lazy" />`
+//           )
+//         )
+//         .join("") || "";
 
-    return `<div class="grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 my-4">${images}</div>`;
-  });
+//     return `<div class="grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 my-4">${images}</div>`;
+//   });
 
-  return updatedHtml;
-}
+//   return updatedHtml;
+// }

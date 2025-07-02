82c660ed6ec53ba53aac476ab1ab22e9437d6b72
LastHolmz <databaserepos@gmail.com>
2025-07-02 15:36:34 +0200

    fix metadata

--- app/[lang]/research-and-studies/layout.tsx
+++ app/[lang]/research-and-studies/layout.tsx
@@ -3,10 +3,58 @@ import { cn } from "@/lib/utils";
 import { Locale } from "@/i18n-config";
 import Header from "../(interface)/components/header";
 import Footer from "../(interface)/components/footer";
+import uri from "@/lib/uri";
 
 const outfit = Outfit({ subsets: ["latin"] });
 const cairo = Cairo({ subsets: ["arabic"] });
+// **2. Generate Metadata**
+export async function generateMetadata(props: {
+  params: Promise<{ lang: string }>;
+}) {
+  const params = await props.params;
+  const lang = params.lang;
 
+  const siteInfo = {
+    ar: {
+      title: "المركز الليبي لبحوث اللدائن | الدراسات و الأنشطة",
+    },
+    en: {
+      title: "Libyan Center for Polymer Research | research and studies",
+    },
+  };
+
+  const info = siteInfo[lang as "ar" | "en"];
+  const logo = "/logo.png"; // Update this path to your actual logo location
+  const url =
+    process.env.NODE_ENV === "production" ? uri : "http://localhost:3000";
+
+  return {
+    title: info.title,
+    // description: info.description,
+    openGraph: {
+      title: info.title,
+      // description: info.description,
+      url: url,
+      images: [
+        {
+          url: logo,
+          alt: info.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: info.title,
+      // description: info.description,
+      images: [logo],
+    },
+    alternates: {
+      canonical: url,
+    },
+
+    // jsonLd: JSON.stringify(jsonLd), // Embeds JSON-LD in the head
+  };
+}
 export default async function RootLayout({
   children,
   params,
